6ed7f42c9aecae6798bbd172d3ecf8eb723c7e07
contact.reob@gmail.com <contact.reob@gmail.com>
2020-05-20 12:07:49 -0300

    Minor updates

--- src/js/reob-containers.js
+++ src/js/reob-containers.js
@@ -24,173 +24,172 @@
      - JS ONLY:
        - REOB-AJAX-REQUEST
    =========================================
-     
+
 */
-     
+
 // REOB-MULTISELECT
 
-class MultiSelect {
-   static addTag(comp) {
-      var s = comp.parentElement;
-      while (!((s.tagName == "DIV") && (s.className == "reob-multiselect"))) {
-         s = s.parentElement;
-      }
-      var t = '<option onclick="MultiSelect.removeTag(this)" value="' + comp.value + '">' + comp.innerText + '</option>';
-      if (!s.children[0].innerHTML.includes(t)) {
-         s.children[0].innerHTML += t;
-      }
-      s.children[2].name = s.id;
-      s.children[2].value = MultiSelect.getValues(s.id);
-      if (s.onchange) s.onchange();
-   }
+class ReobMultiSelect {
+	static addTag ( comp ) {
+		var s = comp.parentElement;
+		while ( !( ( s.tagName == "DIV" ) && ( s.className == "reob-multiselect" ) ) ) {
+			s = s.parentElement;
+		}
+		var t = '<option onclick="MultiSelect.removeTag(this)" value="' + comp.value + '">' + comp.innerText + '</option>';
+		if ( !s.children[ 0 ].innerHTML.includes( t ) ) {
+			s.children[ 0 ].innerHTML += t;
+		}
+		s.children[ 2 ].name = s.id;
+		s.children[ 2 ].value = MultiSelect.getValues( s.id );
+		if ( s.onchange ) s.onchange();
+	}
 
-   static removeTag(comp) {
-      var t = '<option onclick="MultiSelect.removeTag(this)" value="' + comp.value + '">' + comp.innerText + '</option>';
-      var s = comp.parentElement;
-      s.innerHTML = s.innerHTML.replace(t, '');
-      s.children[2].name = s.id;
-      s.children[2].value = MultiSelect.getValues(s.id);
-      if (s.onchange) s.onchange();
-   }
+	static removeTag ( comp ) {
+		var t = '<option onclick="MultiSelect.removeTag(this)" value="' + comp.value + '">' + comp.innerText + '</option>';
+		var s = comp.parentElement;
+		s.innerHTML = s.innerHTML.replace( t, '' );
+		s.children[ 2 ].name = s.id;
+		s.children[ 2 ].value = MultiSelect.getValues( s.id );
+		if ( s.onchange ) s.onchange();
+	}
 
-   static getValues(id) {
-      var c = document.getElementById(id);
-      var l = [];
-      var o = c.children[0].children;
-      for(var i = 0; i < o.length; i++) {
-         l.push(o[i].value);
-      }
-      return JSON.stringify(l);
-   }
+	static getValues ( id ) {
+		var c = document.getElementById( id );
+		var l = [];
+		var o = c.children[ 0 ].children;
+		for ( var i = 0; i < o.length; i++ ) {
+			l.push( o[ i ].value );
+		}
+		return JSON.stringify( l );
+	}
 
-   static selectOption(id, value) {
-      var c = document.getElementById(id);
-      var o = c.children[1].children;
-      for(var i = 0; i < o.length; i++) {
-         if (o[i].tagName == "OPTGROUP") {
-            for(var j = 0; j < o[i].children.length; j++) {
-               if ((o[i].children[j].tagName == "OPTION") && (o[i].children[j].value == value)) {
-                  MultiSelect.addTag(o[i].children[j]);
-               }
-            }
-         } else {
-            if ((o[i].tagName == "OPTION") && (o[i].value == value)) {
-               MultiSelect.addTag(o[i]);
-            }
-         }
-      }
-   }
+	static selectOption ( id, value ) {
+		var c = document.getElementById( id );
+		var o = c.children[ 1 ].children;
+		for ( var i = 0; i < o.length; i++ ) {
+			if ( o[ i ].tagName == "OPTGROUP" ) {
+				for ( var j = 0; j < o[ i ].children.length; j++ ) {
+					if ( ( o[ i ].children[ j ].tagName == "OPTION" ) && ( o[ i ].children[ j ].value == value ) ) {
+						MultiSelect.addTag( o[ i ].children[ j ] );
+					}
+				}
+			} else {
+				if ( ( o[ i ].tagName == "OPTION" ) && ( o[ i ].value == value ) ) {
+					MultiSelect.addTag( o[ i ] );
+				}
+			}
+		}
+	}
 }
 
 // REOB-UNISELECT
 
-class UniSelect {
-   static addTag(comp) {
-      var s = comp.parentElement;
-      while (!((s.tagName == "DIV") && (s.className == "reob-uniselect"))) {
-         s = s.parentElement;
-      }
-      var t = '<option onclick="UniSelect.removeTag(this)" value="' + comp.value + '">' + comp.innerText + '</option>';
-      s.children[0].innerHTML = t;
-      s.children[2].name = s.id;
-      s.children[2].value = UniSelect.getValue(s.id);
-      if (s.onchange) s.onchange();
-   }
- 
-   static removeTag(comp) {
-      var t = '<option onclick="UniSelect.removeTag(this)" value="' + comp.value + '">' + comp.innerText + '</option>';
-      var s = comp.parentElement;
-      s.innerHTML = s.innerHTML.replace(t, '');
-      s.children[2].name = s.id;
-      s.children[2].value = UniSelect.getValue(s.id);
-      if (s.onchange) s.onchange();
-   }
- 
-   static getValue(id) {
-      var c = document.getElementById(id);
-      var o = c.children[0].children;
-      var v = o[0].value;
-      return v;
-   }
- 
-   static selectOption(id, value) {
-      var c = document.getElementById(id);
-      var o = c.children[1].children;
-      for(var i = 0; i < o.length; i++) {
-         if (o[i].tagName == "OPTGROUP") {
-            for(var j = 0; j < o[i].children.length; j++) {
-               if ((o[i].children[j].tagName == "OPTION") && (o[i].children[j].value == value)) {
-                  UniSelect.addTag(o[i].children[j]);
-               }
-            }
-         } else {
-            if ((o[i].tagName == "OPTION") && (o[i].value == value)) {
-               UniSelect.addTag(o[i]);
-            }
-         }
-      }
-   }
+class ReobUniSelect {
+	static addTag ( comp ) {
+		var s = comp.parentElement;
+		while ( !( ( s.tagName == "DIV" ) && ( s.className == "reob-uniselect" ) ) ) {
+			s = s.parentElement;
+		}
+		var t = '<option onclick="UniSelect.removeTag(this)" value="' + comp.value + '">' + comp.innerText + '</option>';
+		s.children[ 0 ].innerHTML = t;
+		s.children[ 2 ].name = s.id;
+		s.children[ 2 ].value = UniSelect.getValue( s.id );
+		if ( s.onchange ) s.onchange();
+	}
+
+	static removeTag ( comp ) {
+		var t = '<option onclick="UniSelect.removeTag(this)" value="' + comp.value + '">' + comp.innerText + '</option>';
+		var s = comp.parentElement;
+		s.innerHTML = s.innerHTML.replace( t, '' );
+		s.children[ 2 ].name = s.id;
+		s.children[ 2 ].value = UniSelect.getValue( s.id );
+		if ( s.onchange ) s.onchange();
+	}
+
+	static getValue ( id ) {
+		var c = document.getElementById( id );
+		var o = c.children[ 0 ].children;
+		var v = o[ 0 ].value;
+		return v;
+	}
+
+	static selectOption ( id, value ) {
+		var c = document.getElementById( id );
+		var o = c.children[ 1 ].children;
+		for ( var i = 0; i < o.length; i++ ) {
+			if ( o[ i ].tagName == "OPTGROUP" ) {
+				for ( var j = 0; j < o[ i ].children.length; j++ ) {
+					if ( ( o[ i ].children[ j ].tagName == "OPTION" ) && ( o[ i ].children[ j ].value == value ) ) {
+						UniSelect.addTag( o[ i ].children[ j ] );
+					}
+				}
+			} else {
+				if ( ( o[ i ].tagName == "OPTION" ) && ( o[ i ].value == value ) ) {
+					UniSelect.addTag( o[ i ] );
+				}
+			}
+		}
+	}
 }
 
 // REOB-PROGRESS-BAR
 
-class ProgressBar {
-   static update(id, value, text = "", include_value = true) {
-      var comp  = document.getElementById(id);
-      value = value.toFixed(1) + "%";
-      if (!(text)) { 
-         text = "Total: " + value;
-      } else {
-         if (include_value) {
-            text += (" " + value);
-         }
-      }
-      comp.children[0].style.width = value;
-      comp.children[1].textContent = text;
-   }
+class ReobProgressBar {
+	static update ( id, value, text = "", include_value = true ) {
+		var comp = document.getElementById( id );
+		value = value.toFixed( 1 ) + "%";
+		if ( !( text ) ) {
+			text = "Total: " + value;
+		} else {
+			if ( include_value ) {
+				text += ( " " + value );
+			}
+		}
+		comp.children[ 0 ].style.width = value;
+		comp.children[ 1 ].textContent = text;
+	}
 }
 
 // REOB-AJAX-REQUEST
 
-class AjaxRequest {
-   constructor(function_ok, function_error = null) {
-      this.function_ok = function_ok;
-      this.function_error = function_error;
-      this.xhttp = new XMLHttpRequest();
-      this.xhttp.withCredentials = true;
-   }
-   
-   doRequest(method, url, header = null, data = null) {
-      var f_ok  = this.function_ok;
-      var f_err = this.function_error;
-      
-      this.xhttp.onreadystatechange = function () {
-         if (this.readyState == 4) {
-            if (this.status == 200) {
-               f_ok(this.responseText);
-            } else {
-               if (f_err) {
-                  if (this.responseText) {
-                     f_err(this.responseText);
-                  }
-               }
-            }
-         }
-      }
-      
-      switch(method) {
-         case 'GET':
-            this.xhttp.open(method, url);
-            this.xhttp.send();
-            break;
-         case 'POST':
-            this.xhttp.open(method, url);
-            if (header) {
-               this.xhttp.setRequestHeader(header[0], header[1]);
-            }
-            this.xhttp.send(data);
-            break;
-      }
-   }
-}
+class ReobAjaxRequest {
+	constructor( function_ok, function_error = null ) {
+		this.function_ok = function_ok;
+		this.function_error = function_error;
+		this.xhttp = new XMLHttpRequest();
+		this.xhttp.withCredentials = true;
+	}
 
+	doRequest ( method, url, header = null, data = null ) {
+		var f_ok = this.function_ok;
+		var f_err = this.function_error;
+
+		this.xhttp.onreadystatechange = function () {
+			if ( this.readyState == 4 ) {
+				if ( this.status == 200 ) {
+					f_ok( this.responseText );
+				} else {
+					if ( f_err ) {
+						if ( this.responseText ) {
+							f_err( this.responseText );
+						}
+					}
+				}
+			}
+		}
+
+		switch ( method ) {
+			case 'GET':
+				this.xhttp.open( method, url );
+				this.xhttp.send();
+				break;
+			case 'POST':
+				this.xhttp.open( method, url );
+				if ( header ) {
+					this.xhttp.setRequestHeader( header[ 0 ], header[ 1 ] );
+				}
+				this.xhttp.send( data );
+				break;
+		}
+	}
+}
